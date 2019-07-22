ea00ae2ed1a07688294039505a34dd0e43603c73
Kevin Hayden <kevin.fhayden@gmail.com>
2019-07-22 23:42:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, element)
+next_color = "violet"
+  ["red", "orange", "yellow", "green", "blue", "indigo"].push(next_color)
+end
+
+def using_unshift(array, element)
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+@new_neighborhood = "Staton Island"
+bouroughs_in_nyc.unshift(@new_neighborhood)
+end
+
+def using_pop(array)
+  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  @continents.pop
+end
+
+def pop_with_args(array)
+  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"].pop(2)
+end
+
+def using_shift(array)
+  
